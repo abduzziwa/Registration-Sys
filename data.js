0a1288081de1b2acb8bfd2a8e58569ce2682987d
Abdul G Zziwa <abdulzziwa@gildeopleidingen.nl>
2024-02-26 10:19:32 +0100

    Trying to solve the logic problem

--- data.js
+++ data.js
@@ -1,22 +1,49 @@
 const prompt = require("prompt-sync")({ sigint: true });
 
-const name = prompt("Wat is je naam? ");
+console.log(new Date());
 
-while ( name || name <= 2 ) {
-    console.log("Vull de naam in eerst")
-    break
-}
-const datum = prompt("Wat is de datum vandaag? ");
-if ( !datum ) {
-    console.log("Vull de datum in")
-}
-const starttijd = prompt("Hoelaat ben je begonnen? ");
-if ( !starttijd ) {
-    console.log("Vull de starttijd in")
-}
-const eindtijd = prompt("Hoe laat ben je klaar? ");
-if ( !eindtijd ) {
-    console.log("Vull de datum in")
+function isValidTimeFormat(time) {
+  const regex = /^([01]\d|2[0-3]):?([0-5]\d)$/;
+  return regex.test(time);
 }
 
+let name;
+while (true) {
+  name = prompt("Wat is je naam? ");
+  if (name && name.length > 2) {
+    break;
+  }
+  console.log("Vul een naam in die langer is dan 2 karakters.");
+}
 
+function registerTime() {
+  let starttijd;
+  while (true) {
+    starttijd = prompt("Hoe laat ben je begonnen ? (HH:MM)");
+    if (isValidTimeFormat(starttijd)) {
+      break;
+    }
+    console.log("Ongeldige tijdnotattie. Voer de de tijd in als HH:MM");
+  }
+
+  let eindtijd;
+  while (true) {
+    eindtijd = prompt("Hoe laat ben je begonnen ? (HH:MM)");
+    if (isValidTimeFormat(eindtijd)) {
+      break;
+    }
+    console.log("Ongeldige tijdnotattie. Voer de de tijd in als HH:MM");
+  }
+
+  while (eindtijd < starttijd) {
+    registerTime();
+    break;
+  }
+}
+
+registerTime();
+
+console.log("Naam:", name);
+console.log("Datum:", new Date());
+console.log("Starttijd:", starttijd);
+console.log("Eindtijd:", eindtijd);
